a8dcb350d392e2ff028e73e8b12b776b56992f9e
Ayush Khatri <khatriayush2003@gmail.com>
2024-12-24 23:03:57 +0530

    Updated navitems.tsx

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -36,31 +36,33 @@ export default function NavItems({ isLoggedIn, user }: { isLoggedIn: boolean, us
   console.log(user);
   return (
     <nav className="border-b">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-0">
         <div className="flex items-center justify-between h-16">
           {/* Mobile menu button */}
-          <Sheet>
-            <SheetTrigger className="lg:hidden">
-              <Menu className="h-6 w-6" />
-            </SheetTrigger>
-            <SheetContent side="left" className="w-[300px]">
-              <div className="mt-6 space-y-4">
-                {categories.map((category) => (
-                  <a
-                    key={category}
-                    href={`#${category.toLowerCase().replace(/\s+/g, '-')}`}
-                    className="block px-4 py-2 text-sm hover:bg-gray-100 rounded-md"
-                  >
-                    {category}
-                  </a>
-                ))}
-              </div>
-            </SheetContent>
-          </Sheet>
+          <div className="flex justify-center items-center gap-3">
+            <Sheet>
+              <SheetTrigger className="lg:hidden">
+                <Menu className="h-6 w-6" />
+              </SheetTrigger>
+              <SheetContent side="left" className="w-[300px]">
+                <div className="mt-6 space-y-4">
+                  {categories.map((category) => (
+                    <a
+                      key={category}
+                      href={`#${category.toLowerCase().replace(/\s+/g, '-')}`}
+                      className="block px-4 py-2 text-sm hover:bg-gray-100 rounded-md"
+                    >
+                      {category}
+                    </a>
+                  ))}
+                </div>
+              </SheetContent>
+            </Sheet>
 
-          {/* Logo */}
-          <div className="flex-shrink-0">
-            <Link href="/" className="text-xl font-bold">PJCollection</Link>
+            {/* Logo */}
+            <div className="flex-shrink-0">
+              <Link href="/" className="text-xl font-bold">PJCollection</Link>
+            </div>
           </div>
 
           {/* Desktop navigation */}
@@ -84,59 +86,61 @@ export default function NavItems({ isLoggedIn, user }: { isLoggedIn: boolean, us
             >
               {isSearchOpen ? <X className="h-5 w-5" /> : <Search className="h-5 w-5" />}
             </button>
-            <Link href="#cart" className="hover:text-gray-600">
-              <ShoppingCart className="h-5 w-5" />
-            </Link>
             {
               isLoggedIn ?
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    {
-                      user?.picture ?
-                        <div className="w-6 h-6 rounded-full overflow-hidden cursor-pointer">
-                          <Image
-                            width={32}
-                            height={32}
-                            src={user.picture}
-                            alt="user"
-                            className="w-full h-full object-cover"
-                          />
+                <div className="flex justify-center items-center flex-row-reverse gap-4">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      {
+                        user?.picture ?
+                          <div className="w-6 h-6 rounded-full overflow-hidden cursor-pointer">
+                            <Image
+                              width={32}
+                              height={32}
+                              src={user.picture}
+                              alt="user"
+                              className="w-full h-full object-cover"
+                            />
 
-                        </div>
-                        :
-                        <div className="cursor-pointer">
-                          <User2Icon size={20} />
-                        </div>
-                    }
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <User />
-                      <span>
-                        Profile
-                      </span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <ShoppingBag />
-                      <span>
-                        Orders
-                      </span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <Heart />
-                      <span>
-                        Orders
-                      </span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className="group text-red-600 hover:bg">
-                      <LogoutLink className="flex justify-center items-center gap-2" postLogoutRedirectURL="/">
-                        Logout <LogOut size={16} />
-                      </LogoutLink>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                          </div>
+                          :
+                          <div className="cursor-pointer">
+                            <User2Icon size={20} />
+                          </div>
+                      }
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <User />
+                        <span>
+                          Profile
+                        </span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <ShoppingBag />
+                        <span>
+                          My Orders
+                        </span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Heart />
+                        <span>
+                          My Wishlist
+                        </span>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem asChild className="group">
+                        <LogoutLink className="flex justify-center group-hover:bg-red-700 items-center text-red-500 gap-2" postLogoutRedirectURL="/">
+                          Logout <LogOut size={16} />
+                        </LogoutLink>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  <Link href="#cart" className="hover:text-gray-600">
+                    <ShoppingCart className="h-5 w-5" />
+                  </Link>
+                </div>
                 :
                 <div className='flex gap-2 md:pl-5 justify-center items-center'>
                   <Button asChild variant="outline">
