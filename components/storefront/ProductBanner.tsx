4c384aaa9b5b9576a2990375aedfabf9598454f3
Ayush Khatri <khatriayush2003@gmail.com>
2024-12-30 22:07:28 +0530

    Fix: Product banner

--- components/storefront/ProductBanner.tsx
+++ components/storefront/ProductBanner.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 
-
-const bannerImg = "https://placehold.co/1992x700"
+const bannerImg = 'https://images.unsplash.com/photo-1465408953385-7c4627c29435?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 export default function ProductBanner() {
   return (
-    <section className="w-full h-auto pb-10 overflow-hidden">
-      <Link href={`/#`}>
-        <div className="">
+    <section className="absolute left-0 w-full h-auto pb-10 overflow-hidden">
+      <Link href="/">
+        <div className="h-[50vh] xs:h-[55vh] sm:h-[60vh] md:h-[70vh] lg:h-[90vh] w-full">
           <img
             src={bannerImg}
-            alt={"banner-img"}
-            className="h-auto w-full mx-auto" />
+            alt=""
+            className="object-cover w-full h-full"
+          />
         </div>
       </Link>
     </section>
